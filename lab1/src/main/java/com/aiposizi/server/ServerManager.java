979252484eedfd737416e1d3a73637d3a5e87870
Brunner20 <antonbrunner3@gmail.com>
2021-02-11 11:48:15 +0300

    parsing method type

--- lab1/src/main/java/com/aiposizi/server/ServerManager.java
+++ lab1/src/main/java/com/aiposizi/server/ServerManager.java
@@ -6,6 +6,11 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
 
 public class ServerManager implements Runnable{
 
@@ -26,12 +31,62 @@ public class ServerManager implements Runnable{
 
     @Override
     public void run() {
-
+        String fileRequested;
         try {
+
             String input = in.readLine();
             logger.log(Level.INFO,"Input:\n"+input);
-        } catch (IOException e) {
+
+            String[] tokens = input.split("\\s");
+            String method = tokens[0].toUpperCase();
+            logger.log(Level.INFO, "Request type: " + method);
+            fileRequested = tokens[1];
+            switch (method){
+                case "GET":
+                    processGet(fileRequested);
+                    break;
+                case "POST":
+                    processPost(fileRequested);
+                    break;
+                case "OPTIONS":
+                    processOptions(fileRequested);
+                    break;
+                default:
+                    methodNotFound(fileRequested);
+            }
+
+        } catch (IOException|NullPointerException e) {
             logger.log(Level.ERROR, "Server error : " + e);
+        }finally {
+            try {
+                in.close();
+            }
+            catch (IOException e) {
+            logger.log(Level.ERROR,"Error closing stream"+e.getMessage()); }
+            try {
+                out.close();
+            } catch (IOException e) {
+                logger.log(Level.ERROR,"Error closing stream"+e.getMessage()); }
+            try {
+                connect.close();
+            } catch (IOException e) {
+                logger.log(Level.ERROR,"Error closing connect"+e.getMessage()); }
+
+            logger.log(Level.INFO,"Connection closed");
+
         }
+    }
+
+    private void methodNotFound(String fileRequested) {
+    }
+
+    private void processOptions(String fileRequested) {
+    }
+
+
+    private void processPost(String fileRequested) {
+    }
+
+    private void processGet(String fileRequested) {
     }
 }
